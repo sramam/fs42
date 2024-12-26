16e0758694bdb00b7fd81e6a70f4908c6d2e086a
Shishir Ramam <sramam@gmail.com>
2024-12-26 09:40:44 -0800

    fixing package.json import

--- index.js
+++ index.js
@@ -1,55 +1,68 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander'
-import { mergeFiles } from './merge.js'
-import { splitFiles } from './split.js'
-import { resolve } from 'path'
-import chalk from 'chalk'
-import { name, version, description } from './package.json' assert { type: 'json' };
+import { Command } from "commander";
+import { mergeFiles } from "./merge.js";
+import { splitFiles } from "./split.js";
+import { resolve } from "path";
+import chalk from "chalk";
+import fs from "fs/promises";
 
-const program = new Command()
+async function main() {
+  const packageJsonPath = resolve("./package.json");
+  const packageJsonData = JSON.parse(
+    await fs.readFile(packageJsonPath, "utf-8")
+  );
+  const { name, version, description } = packageJsonData;
 
-program
-  .name(name)
-  .description(description)
-  .version(version)
+  const program = new Command();
 
-program
-  .command('merge')
-  .description('Merge multiple files into a single file with markers')
-  .argument('<paths>', 'Comma-separated list of files/directories to merge')
-  .option('-i, --ignore <patterns>', 'Comma-separated patterns to ignore', '')
-  .option('-r, --root-dir <dir>', 'Root directory for relative paths', process.cwd())
-  .action(async (paths, options) => {
-    try {
-      const pathList = paths.split(',').map(p => p.trim())
-      const ignorePatterns = options.ignore ? options.ignore.split(',').map(p => p.trim()) : []
-      const rootDir = resolve(options.rootDir)
-      
-      const result = await mergeFiles(pathList, {
-        ignorePatterns,
-        rootDir
-      })
-      
-      process.stdout.write(result)
-    } catch (error) {
-      console.error(chalk.red('Error:'), error.message)
-      process.exit(1)
-    }
-  })
+  program.name(name).description(description).version(version);
 
-program
-  .command('split')
-  .description('Split a merged file back into individual files')
-  .argument('<outputDir>', 'Directory to output split files')
-  .argument('<mergeFile>', 'File containing merged content')
-  .action(async (outputDir, mergeFile) => {
-    try {
-      await splitFiles(outputDir, mergeFile)
-    } catch (error) {
-      console.error(chalk.red('Error:'), error.message)
-      process.exit(1)
-    }
-  })
+  program
+    .command("merge")
+    .description("Merge multiple files into a single file with markers")
+    .argument("<paths>", "Comma-separated list of files/directories to merge")
+    .option("-i, --ignore <patterns>", "Comma-separated patterns to ignore", "")
+    .option(
+      "-r, --root-dir <dir>",
+      "Root directory for relative paths",
+      process.cwd()
+    )
+    .action(async (paths, options) => {
+      try {
+        const pathList = paths.split(",").map((p) => p.trim());
+        const ignorePatterns = options.ignore
+          ? options.ignore.split(",").map((p) => p.trim())
+          : [];
+        const rootDir = resolve(options.rootDir);
 
-program.parse()
+        const result = await mergeFiles(pathList, {
+          ignorePatterns,
+          rootDir,
+        });
+
+        process.stdout.write(result);
+      } catch (error) {
+        console.error(chalk.red("Error:"), error.message);
+        process.exit(1);
+      }
+    });
+
+  program
+    .command("split")
+    .description("Split a merged file back into individual files")
+    .argument("<outputDir>", "Directory to output split files")
+    .argument("<mergeFile>", "File containing merged content")
+    .action(async (outputDir, mergeFile) => {
+      try {
+        await splitFiles(outputDir, mergeFile);
+      } catch (error) {
+        console.error(chalk.red("Error:"), error.message);
+        process.exit(1);
+      }
+    });
+
+  program.parse();
+}
+
+main();
